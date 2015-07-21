18fb7dcbc0914c2fb21f6f4a20beff5c32811623
Kostadin Stoilov <kmstoilov@gmail.com>
2015-07-21 10:57:25 +0300

    ADD: maven repo initial setup

---
+++ src/main/java/com/ontotext/kstoilov/interview/task2/App.java
@@ -0,0 +1,13 @@
+package com.ontotext.kstoilov.interview.task2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
